f69acbca808713797554ad0f9006c9ab02bf3731
Prachi Vyas <prachiv220@gmail.com>
2026-09-11 18:22:50 +0300

    add module 1&2

---
+++ module 1/myfile.py
@@ -0,0 +1,32 @@
+print("Hello")
+
+print('"Hello", said Prachi')
+
+print('"I\'m Prachi"')
+
+print(50)
+print(5)
+
+print("Good")
+print("Morning")
+
+print("Good\nMorning")
+
+
+
+
+name = input("Name:")
+
+
+print(name)
+print("name")
+print("Your name is "+ name)
+
+
+
+points = 50  # points is now 50
+print(points)  # prints: 50
+
+points = 120  # now points is 120
+print(points)  # prints: 120
+ # hastag is used for comment it wont be printed and it appears in grey color
